398a6d82d8b6f45fc41235ed5ce7b6f9d5de307c
Bryan <bryancombs@msn.com>
2020-10-08 12:04:40 -0600

    modified code to write file from to pages in csv form that could be imported by pandas'

--- book_scrape/book_scrape/spiders/book_spyder.py
+++ book_scrape/book_scrape/spiders/book_spyder.py
@@ -5,7 +5,8 @@ class BooksSpider(scrapy.Spider):
     name = 'books'
     start_urls = [
 
-        'http://books.toscrape.com/'
+        'http://books.toscrape.com/',
+        'http://books.toscrape.com/catalogue/category/books/fantasy_19/index.html'
     ]
 
     #scraping the entire page
@@ -14,9 +15,16 @@ class BooksSpider(scrapy.Spider):
     #     filename = 'books-%s.html' % page
     #     with open(filename, 'wb') as f:
     #         f.write(response.body)
+
     def parse(self, response):
         item = BookScrapeItem()
         item['title']= response.xpath('//*/h3/a/text()').extract()
         item['price']= response.xpath('//p[@class="price_color"]/text()').extract()
+        rows = list(zip(item['title'], item['price']))
+        with open('books.csv', 'a') as f:
+            #f.write("Title,Price\n")
+            for row in rows:
+                f.write(f'{row[0]},{row[1]}\n')
+                
 
-        return item
+        return None
